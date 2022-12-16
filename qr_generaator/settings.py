ad5a23d4202a22977cea48e001659f4364425e52
Popsicool <akinolasamson1234@gmail.com>
2022-12-16 19:30:12 +0100

    settings for render

--- qr_generaator/settings.py
+++ qr_generaator/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'generator.User'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -154,5 +155,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 # DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
